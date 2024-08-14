56774eb4f2e6eb576d0b0ca423ce44b0291b811b
LavishT04 <lavishtandon04@gmail.com>
2024-08-14 17:40:17 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const humberger = document.getElementById('humberger1');
+const menu = document.getElementById('menu1');
+
+humberger.addEventListener('click', ()=>
+    {menu.classList.toggle('show')
+
+    });
